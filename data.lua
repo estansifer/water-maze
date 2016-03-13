7dd9835464a2c27a45592f188988d9d01defa2f9
Eric Stansifer <eric.stansifer+git@gmail.com>
2016-03-13 14:52:53 -0400

    Fixed problem with Factorio trying to spawn water
    This resolves a number of strange artifacts previously observed.

---
+++ data.lua
@@ -0,0 +1,38 @@
+-- Copied from data/base/prototypes/tile/tiles.lua
+function water_autoplace_settings(from_depth, rectangles)
+  local ret =
+  {
+    {
+      -- Water and deep water have absolute priority. We simulate this by giving
+      -- them absurdly large influence
+      influence = 1e3 + from_depth,
+      elevation_optimal = -5000 - from_depth,
+      elevation_range = 5000,
+      elevation_max_range = 5000, -- everywhere below elevation 0 and nowhere else
+    }
+  }
+
+  if rectangles == nil then
+    ret[2] = { influence = 1 }
+  end
+
+  -- autoplace_utils.peaks(rectangles, ret)
+
+  return { peaks = ret }
+end
+
+print(data)
+print(data.raw)
+print(data.raw.tile)
+
+for a, b in pairs(data.raw) do
+    print ('   ' .. tostring(a) .. ' ' .. tostring(b))
+end
+
+local t = data.raw.tile
+local x = water_autoplace_settings(10000)
+
+t.water.autoplace = x
+t.deepwater.autoplace = x
+t['water-green'].autoplace = x
+t['deepwater-green'].autoplace = x
