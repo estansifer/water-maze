1b4dd814a0d27a2f818a68542712f61ac51fc99b
Eric Stansifer <eric.stansifer+git@gmail.com>
2016-03-26 07:11:18 -0400

    Improved memory characteristics of queue

--- queue.lua
+++ queue.lua
@@ -9,8 +9,10 @@ local function empty ()
         tail = tail + 1
     end
     local function pop()
+        local value = elems[head]
+        elems[head] = nil   -- help the garbage collector
         head = head + 1
-        return elems[head - 1]
+        return value
     end
     local function size()
         return tail - head
